e6f80640a32fbbf94bcc4fee14b6132b7df4d01b
Anton Demchenko <d.flier@yandex.ru>
2019-04-15 13:15:08 +0200

    Add authentication middleware

--- src/middleware/__init__.py
+++ src/middleware/__init__.py
@@ -1,7 +1,9 @@
 from .exception import exception_middleware
+from .login import login_middleware
 
 MIDDLEWARES = [
-    exception_middleware
+    exception_middleware,
+    login_middleware
 ]
 
 __all__ = [
